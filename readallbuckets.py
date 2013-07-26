e16687398eee366c41fef50aa1475db2581477b9
Nagendra Medida <nmedida@netflix.com>
2013-07-26 13:25:47 -0700

    Read all the buckets

---
+++ readallbuckets.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+"""
+- Author : Nag m
+- Lists all the buckets 
+"""
+
+import boto
+
+def getallbuckets():
+   buckets = conn.get_all_buckets()
+   for b buckets:
+       print b.name
+
+if __name__ == "__main__":
+   conn = boto.connect_s3()
+   getallbuckets()
