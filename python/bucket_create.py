a0601e27f54fb41d4256768242e714b0c8c45fae
Nag Medida <nmedida@netflix.com>
2017-03-09 16:39:30 -0800

    Create an S3 bucket

--- python/bucket_create.py
+++ python/bucket_create.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
 """
-- Author : Nag m
-- Hack   : Create an Bucket in S3
+- Author  : Nag m
+- Hack    : Create an Bucket in S3
+- AWS CLI : aws s3api create-bucket  --bucket us-east-1.nag
 """
 
 import boto3
 
 if __name__ == "__main__":
    client = boto3.client('s3')
-   bucketname = "101-s3-aws-1"
+   bucketname = "us-east-1.nag"
    print client.create_bucket(Bucket=bucketname)
