b1038520db73f593ba355ed89c6821e2974f9385
Nag Medida <nmedida@netflix.com>
2017-03-09 16:51:34 -0800

    Create an S3 bucket in a region

--- python/bucket_create_byregion.py
+++ python/bucket_create_byregion.py
@@ -3,11 +3,13 @@
 """
 - Author : Nag m
 - Hack   : Create an Bucket in S3
+- AWS CLI : aws s3api create-bucket  --bucket us-west-1.nag --region us-west-1 --create-bucket-configuration LocationConstraint=us-west-1
+
 """
 
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3')
-   bucketname = "eu-west-1.nag1"
-   print client.create_bucket(Bucket=bucketname, CreateBucketConfiguration={ 'LocationConstraint': 'us-west-1' })
+    client = boto3.client('s3')
+    bucketname = "us-west-1.nag"
+    print client.create_bucket(Bucket=bucketname, CreateBucketConfiguration={'LocationConstraint': 'us-west-1'})
