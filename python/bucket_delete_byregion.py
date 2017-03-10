04d666bfb03a412ce8283b196e9ee6692a58c85c
Nag Medida <nmedida@netflix.com>
2017-03-09 16:59:38 -0800

    Delete an S3 bucket by region

--- python/bucket_delete_byregion.py
+++ python/bucket_delete_byregion.py
@@ -3,11 +3,12 @@
 """
 - Author : Nag m
 - Hack   : Delete an Bucket in S3
+- AWS CLI: aws s3api delete-bucket --bucket us-west-1.nag ( You do not have to specify the region when deleting the bucket )
 """
 
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3',region_name="eu-west-1")
-   bucket_name = "eu-west-1.nag"
-   print client.delete_bucket(Bucket=bucket_name)
+    client = boto3.client('s3', region_name="us-west-1")
+    bucket_name = "us-west-1.nag"
+    print client.delete_bucket(Bucket=bucket_name)
