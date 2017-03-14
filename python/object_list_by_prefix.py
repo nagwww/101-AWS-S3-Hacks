8ed50f755ae122d932afc8bb51579b42dbf0f979
Nag Medida <nmedida@netflix.com>
2017-03-14 16:24:18 -0700

    List all the objects by path

--- python/object_list_by_prefix.py
+++ python/object_list_by_prefix.py
@@ -2,14 +2,15 @@
 
 """
 - Author : Nag m
-- Hack   : Create an Object in S3
+- Hack   : List all the objects by path
+- AWS CLI: aws s3 ls s3://us-west-2.nag/hello/
 """
 
 import json
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3',region_name="us-west-2")
-   bucketname = "us-west-2.nag"
-   for obj in client.list_objects(Bucket=bucketname, Prefix="AWSLogs/")["Contents"]:
-       print obj["Key"]
+    client = boto3.client('s3', region_name="us-west-2")
+    bucketname = "us-west-2.nag"
+    for obj in client.list_objects(Bucket=bucketname, Prefix="hello/")["Contents"]:
+        print obj["Key"]
