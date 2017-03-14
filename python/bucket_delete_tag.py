b627d39d90e6dac71f2edd05f43914470fc1b5c0
Nag Medida <nmedida@netflix.com>
2017-03-14 15:58:23 -0700

    Delete bucket tagging

--- python/bucket_delete_tag.py
+++ python/bucket_delete_tag.py
@@ -2,12 +2,13 @@
 
 """
 - Author : Nag m
-- Hack   : Create an Bucket in S3
+- Hack   : Delete bucket tagging
+- AWS CLI: aws s3api delete-bucket-tagging --bucket us-west-2.nag
 """
 
 import boto3
 
 if __name__ == "__main__":
    client = boto3.client('s3')
-   bucketname = "101-s3-aws"
+   bucketname = "us-west-2.nag"
    print client.delete_bucket_tagging(Bucket=bucketname)
