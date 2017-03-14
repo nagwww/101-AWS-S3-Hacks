82a7cc372d65e73ca3b15c1e513930c25fb40e23
Nag Medida <nmedida@netflix.com>
2017-03-14 16:09:45 -0700

    Create an Object in S3

--- python/object_list.py
+++ python/object_list.py
@@ -3,13 +3,14 @@
 """
 - Author : Nag m
 - Hack   : Create an Object in S3
+- AWS CLI: aws s3 ls s3://us-west-2.nag
 """
 
 import json
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3',region_name="us-west-2")
-   bucketname = "us-west-2.nag"
-   for obj in client.list_objects(Bucket=bucketname)["Contents"]:
-       print obj["Key"]
+    client = boto3.client('s3', region_name="us-west-2")
+    bucketname = "us-west-2.nag"
+    for obj in client.list_objects(Bucket=bucketname)["Contents"]:
+        print obj["Key"]
