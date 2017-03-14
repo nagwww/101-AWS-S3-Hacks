1dc502125c166a8041e2540d6f95d3b9529e887b
Nag Medida <nmedida@netflix.com>
2017-03-13 18:43:57 -0700

    To delete an S3 bucket

--- python/bucket_delete.py
+++ python/bucket_delete.py
@@ -3,11 +3,12 @@
 """
 - Author : Nag m
 - Hack   : Delete an Bucket in S3
+- AWS CLI: aws s3api delete-bucket --bucket us-east-1.nag
 """
 
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3')
-   bucket_name = "101-s3-aws-1"
-   print client.delete_bucket(Bucket=bucket_name)
+    client = boto3.client('s3')
+    bucket_name = "us-east-1.nag"
+    print client.delete_bucket(Bucket=bucket_name)
