e8ff1a42303f9ec57d0bb2036a901ac9dc5de553
Nag Medida <nmedida@netflix.com>
2017-03-14 16:12:34 -0700

    Delete an Object in S3

--- python/object_delete.py
+++ python/object_delete.py
@@ -3,11 +3,12 @@
 """
 - Author : Nag m
 - Hack   : Delete an Object in S3
+- AWS CLI: aws s3api delete-object --bucket us-west-2.nag --key hello.txt
 """
 
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3')
-   bucketname = "101-s3-aws"
-   print client.delete_object(Bucket=bucketname, Key="hello.txt")
+    client = boto3.client('s3')
+    bucketname = "us-west-2.nag"
+    print client.delete_object(Bucket=bucketname, Key="hello.txt")
