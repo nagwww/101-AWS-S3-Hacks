688bcc92a29e40297cf3445777a10743d1c83ba6
Nag Medida <nmedida@netflix.com>
2017-03-14 15:53:52 -0700

    Get all the tags for an S3 bucket

--- python/bucket_get_tag.py
+++ python/bucket_get_tag.py
@@ -2,12 +2,13 @@
 
 """
 - Author : Nag m
-- Hack   : Create an Bucket in S3
+- Hack   : Get all the tags for an S3 bucket
+- AWC CLI: aws s3api get-bucket-tagging --bucket us-west-2.nag
 """
 
 import boto3
 
 if __name__ == "__main__":
    client = boto3.client('s3')
-   bucketname = "101-s3-aws"
+   bucketname = "us-west-2.nag"
    print client.get_bucket_tagging(Bucket=bucketname)["TagSet"]
