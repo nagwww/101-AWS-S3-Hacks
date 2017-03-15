621d14082351637bd17c6637beba08943bff2bf3
Nag Medida <nmedida@netflix.com>
2017-03-14 20:00:05 -0700

    Tag an s3 object

--- python/object_put_tag.py
+++ python/object_put_tag.py
@@ -2,13 +2,14 @@
 
 """
 - Author : Nag m
-- Hack   : Create an Object in S3
+- Hack   : Tag an S3 object
+- AWS CLI: aws s3api put-object-tagging  --bucket us-west-2.nag --key hello.txt --tagging 'TagSet=[{Key=name,Value=nag}]'
 """
 
-import json
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3')
-   bucketname = "101-s3-aws"
-   print client.put_object_tagging(Bucket=bucketname, Key="hello.txt",  Tagging={ "TagSet": [ { "Key": "Name", "Value": "Nag" }, ] })
+    client = boto3.client('s3')
+    bucketname = "us-west-2.nag"
+    print client.put_object_tagging(Bucket=bucketname, Key="hello.txt",
+                                    Tagging={"TagSet": [{"Key": "Name", "Value": "Nag"}, ]})
