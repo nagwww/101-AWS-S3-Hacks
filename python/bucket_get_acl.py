a17e4fc70fd44b422485c559cb3fc7d965e56e25
Nag Medida <nmedida@netflix.com>
2017-03-16 14:35:21 -0700

    Get all the ACL's for an S3 bucket

--- python/bucket_get_acl.py
+++ python/bucket_get_acl.py
@@ -2,13 +2,14 @@
 
 """
 - Author : Nag m
-- Hack   : Create an Bucket in S3
+- Hack   : Get an ACL for a bucket
+- AWS CLI: aws s3api get-bucket-acl --bucket us-west-2.nag
 """
 
 import json
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3')
-   bucketname = "us-west-2.nag"
-   print json.dumps(client.get_bucket_acl(Bucket=bucketname)["Grants"], indent=1)
+    client = boto3.client('s3')
+    bucketname = "us-west-2.nag"
+    print json.dumps(client.get_bucket_acl(Bucket=bucketname)["Grants"], indent=1)
