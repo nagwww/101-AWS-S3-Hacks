e407cc715be6fb9e62135b4b1cff5a8f9f5ed9bb
Nag Medida <nmedida@netflix.com>
2017-04-13 16:30:38 -0700

    Create a bucket policy

---
+++ python/bucket_put_policy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+- Author : Nag m
+- Hack   : Create bucket policy
+- AWS CLI: aws s3api put-bucket-policy --bucket us-west-2.nag --policy '{"Version":"2012-10-17","Statement":[{"Sid":"AddPerm","Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::us-west-2.nag/*"}]}'
+"""
+
+import json
+import boto3
+p = {"Version":"2012-10-17","Statement":[{"Sid":"AddPerm","Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::us-west-2.nag/*"}]}
+
+if __name__ == "__main__":
+    client = boto3.client('s3')
+    bucketname = "us-west-2.nag"
+    print client.put_bucket_policy(Bucket=bucketname, Policy=json.dumps(p))
