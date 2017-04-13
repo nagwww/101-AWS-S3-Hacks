458b2ee66a06e22122bef7ff512a4dae8b0aeaf7
Nag Medida <nmedida@netflix.com>
2017-04-13 16:03:39 -0700

    Delete object tagging

--- python/object_delete_tag.py
+++ python/object_delete_tag.py
@@ -2,13 +2,13 @@
 
 """
 - Author : Nag m
-- Hack   : Create an Object in S3
+- Hack   : Delete object tagging
+- AWS CLI: aws s3api delete-object-tagging --bucket us-west-2.nag --key hello.txt
 """
 
-import json
 import boto3
 
 if __name__ == "__main__":
-   client = boto3.client('s3')
-   bucketname = "101-s3-aws"
-   print client.delete_object_tagging(Bucket=bucketname, Key="hello.txt")
+    client = boto3.client('s3')
+    bucketname = "us-west-2.nag"
+    print client.delete_object_tagging(Bucket=bucketname, Key="hello.txt")
