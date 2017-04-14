01f54ba6f533754e4f33063bfa0d7d73102c29f6
Nag Medida <nmedida@netflix.com>
2017-04-14 16:58:41 -0700

    Suspend versioning for an S3 bucket

---
+++ python/bucket_suspend_versioning.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+"""
+- Author : Nag m
+- Hack   : Suspend versioning for an S3 bucket
+- AWS CLI: aws s3api put-bucket-versioning --bucket us-west-2.nag --versioning-configuration '{"Status":"Suspended"}'
+"""
+
+import json
+import boto3
+
+v={
+        'Status': 'Suspended'
+    }
+
+if __name__ == "__main__":
+    client = boto3.client('s3')
+    bucketname = "us-west-2.nag"
+    print client.put_bucket_versioning(Bucket=bucketname, VersioningConfiguration=v)
