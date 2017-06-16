2306f94169a9deca0c9c05ae3433a3fcbc56fa2f
Nag Medida <nmedida@netflix.com>
2017-06-16 16:33:58 -0700

    Bucket : Get bucket inventory

---
+++ python/bucket_get_inventory.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+"""
+- Author : Nag m
+- Hack   : Get the inventory configurations for an S3 bucket.
+- AWS CLI: aws s3api get-bucket-inventory-configurations --bucket us-west-2.nag
+"""
+
+import boto3
+
+if __name__ == "__main__":
+    client = boto3.client('s3')
+    bucketname = "us-west-2.nag"
+    analytics_config = client.get_bucket_inventory_configuration(Bucket=bucketname, Id="nagbucket")
+    print analytics_config["InventoryConfiguration"]
