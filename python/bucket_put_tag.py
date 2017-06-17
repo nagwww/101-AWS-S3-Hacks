cca2c32a6defe381a230a90314a1c50b80203970
Nag Medida <nmedida@netflix.com>
2017-06-16 23:40:19 -0700

    Bucket : Tag an s3 bucket

--- python/bucket_put_tag.py
+++ python/bucket_put_tag.py
@@ -9,10 +9,10 @@
 import boto3
 
 if __name__ == "__main__":
-#   client = boto3.client('s3')
-   client = boto3.client('s3',region_name="us-west-2")
-   bucketname = "us-west-2.nag"
-   print client.put_bucket_tagging(Bucket=bucketname,Tagging={
+    #   client = boto3.client('s3')
+    client = boto3.client('s3', region_name="us-west-2")
+    bucketname = "us-west-2.nag"
+    print client.put_bucket_tagging(Bucket=bucketname, Tagging={
         'TagSet': [
             {
                 'Key': 'Name',
