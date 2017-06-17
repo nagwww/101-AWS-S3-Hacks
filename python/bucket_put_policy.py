2247f68161f9c7818c66ec0917ebb433f76a35ac
Nag Medida <nmedida@netflix.com>
2017-06-16 23:39:45 -0700

    Bucket : Create bucket policy

--- python/bucket_put_policy.py
+++ python/bucket_put_policy.py
@@ -8,7 +8,10 @@
 
 import json
 import boto3
-p = {"Version":"2012-10-17","Statement":[{"Sid":"AddPerm","Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::us-west-2.nag/*"}]}
+
+p = {"Version": "2012-10-17", "Statement": [
+    {"Sid": "AddPerm", "Effect": "Allow", "Principal": "*", "Action": "s3:GetObject",
+     "Resource": "arn:aws:s3:::us-west-2.nag/*"}]}
 
 if __name__ == "__main__":
     client = boto3.client('s3')
